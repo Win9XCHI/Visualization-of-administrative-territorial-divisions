ffb058da2b8566cc744e4e296d5d42e11f27bad6
unknown <nikulicov@gmail.com>
2020-05-01 20:32:58 +0300

    Added main project and connect to database

---
+++ Handler/Handler/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Handler.Models;
+
+namespace Handler.Controllers {
+    public class HomeController : Controller {
+        IUserRepository repo;
+        public HomeController(IUserRepository r) {
+            repo = r;
+        }
+        public ActionResult Index() {
+            return View(repo.GetUsers());
+        }
+
+        public ActionResult Details(int id) {
+            Input user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Input user) {
+            repo.Create(user);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id) {
+            Input user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Input user) {
+            repo.Update(user);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id) {
+            Input user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id) {
+            repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
