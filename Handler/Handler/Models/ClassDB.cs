a0afc9d7f240aac4157b5c2da9b2fbf3d1918e65
unknown <nikulicov@gmail.com>
2020-05-12 16:19:44 +0300

    Refactoring class for database

--- Handler/Handler/Models/ClassDB.cs
+++ Handler/Handler/Models/ClassDB.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Dapper;
+﻿using Dapper;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data;
@@ -16,150 +15,107 @@ namespace Handler.Models {
         {
             connectionString = conn;
         }
-
         public void INSERT(string table_name, ArrayList listColumns, ArrayList listValue)
         {
-            string Columns = "";
-            string Values = "";
-
-            for (int i = 0; i < listColumns.Count; i++)
+            if (table_name.Length != 0 && listColumns.Count != 0 && listColumns.Count == listValue.Count)
             {
-                Columns += listColumns[i].ToString();
+                string Columns = "";
+                string Values = "";
 
-                if (i != listColumns.Count - 1)
+                for (int i = 0; i < listColumns.Count; i++)
                 {
-                    Columns += ", ";
+                    Columns += listColumns[i].ToString();
+
+                    if (i != listColumns.Count - 1)
+                    {
+                        Columns += ", ";
+                    }
                 }
-            }
 
-            for (int i = 0; i < listValue.Count; i++)
-            {
-                Values += listValue[i].ToString();
-
-                if (i != listValue.Count - 1)
+                for (int i = 0; i < listValue.Count; i++)
                 {
-                    Values += ", ";
-                }
-            }
+                    Values += listValue[i].ToString();
 
-            InsertP(table_name, Columns, Values);
+                    if (i != listValue.Count - 1)
+                    {
+                        Values += ", ";
+                    }
+                }
+
+                InsertP(table_name, Columns, Values);
+            }
         }
         private void InsertP(string table_name, string listColumns, string listValue)
         {
-
             using (IDbConnection db = new SqlConnection(connectionString))
             {
-
-                if (table_name != "" && listValue != "" && listColumns != "")
-                {
-                    db.Execute("INSERT INTO " + table_name + " (" + listColumns + ") VALUES (" + listValue + ");");
-                }
+                db.Execute("INSERT INTO " + table_name + " (" + listColumns + ") VALUES (" + listValue + ");");
             }
         }
-
-        public void UPDATE(string table, string value, string definition = "")
+        public void UPDATE(string table_name, string value, string definition = "")
         {
-
-            using (IDbConnection db = new SqlConnection(connectionString))
+            if (table_name.Length != 0 && value.Length != 0)
             {
-
-                if (table != "" && value != "")
-                {
-
-                    if (definition != "")
-                    {
-                        db.Execute("UPDATE " + table + " SET " + value + " WHERE " + definition + ";");
-                    }
-                    else
-                    {
-                        db.Execute("UPDATE " + table + " SET " + value + ";");
-                    }
-                }
-            }
-        }
-
-        public List<T> SELECT<T>(string column, string table, string definition = "", string limit = "", string Order_by = "")
-        {
-
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                if (table != "")
-                {
-
-                    if (limit == "")
-                    {
-
-                        if (Order_by == "")
-                        {
-                            if (definition == "")
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " ;").ToList();
-                            }
-                            else
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " WHERE " + definition + " ;").ToList();
-                            }
-                        }
-                        else
-                        {
-                            if (definition == "")
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " ORDER BY " + Order_by + ";").ToList();
-                            }
-                            else
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " WHERE " + definition + " ORDER BY " + Order_by + ";").ToList();
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (Order_by == "")
-                        {
-                            if (definition == "")
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " LIMIT " + limit + " ;").ToList();
-                            }
-                            else
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " WHERE " + definition + " LIMIT " + limit + ";").ToList();
-                            }
-                        }
-                        else
-                        {
-                            if (definition == "")
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " LIMIT " + limit + " ORDER BY " + Order_by + ";").ToList();
-                            }
-                            else
-                            {
-                                return db.Query<T>("SELECT " + column + " FROM " + table + " WHERE " + definition + " LIMIT " + limit + " ORDER BY " + Order_by + ";").ToList();
-                            }
-                        }
-                    }
-                }
-                List<T> NullList = new List<T>();
-                return NullList;
-            }
-        }
-
-        public void DELETE(string table_name, string definition = "")
-        {
-            if (table_name.Length > 0)
-            {
-
                 using (IDbConnection db = new SqlConnection(connectionString))
                 {
-
-                    if (definition.Length > 0)
+                    string str = "UPDATE " + table_name + " SET " + value;
+                    if (definition != "")
                     {
+                        str += " WHERE " + definition;
+                    }
+                    str += ";";
+                    db.Execute(str);
+                }
+            }
+        }
+        public List<T> SELECT<T>(string columns, string table_name, string definition = "", string Group_by = "", string Order_by = "", string Having = "")
+        {
+            if (table_name.Length == 0 || columns.Length == 0)
+            {
+                throw new System.Exception("Null table_name or columns ");
+            }
 
-                        db.Execute("DELETE FROM " + table_name + ";");
-                    }
-                    else
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                //
+                string str = "SELECT " + columns + " FROM " + table_name;
+
+                if (definition.Length != 0) {
+                    str += " WHERE " + definition;
+                }
+
+                if (Group_by.Length != 0)
+                {
+                    str += " GROUP BY " + Group_by;
+                }
+
+                if (Order_by.Length != 0)
+                {
+                    str += " ORDER BY " + Order_by;
+                }
+
+                if (Having.Length != 0)
+                {
+                    str += " HAVING " + Having;
+                }
+
+                str += ";";
+                return db.Query<T>(str).ToList();
+            }
+        }
+        public void DELETE(string table_name, string definition = "")
+        {
+            if (table_name.Length != 0)
+            {
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+                    string str = "DELETE FROM " + table_name;
+                    if (definition.Length != 0)
                     {
-                        db.Execute("DELETE FROM " + table_name + " WHERE " + definition + ";");
+                        str += " WHERE " + definition;
                     }
+                    str += ";";
+                    db.Execute(str);
                 }
             }
         }
--- Handler/Handler/Models/Interface.cs
+++ Handler/Handler/Models/Interface.cs
@@ -7,7 +7,7 @@ namespace Handler.Models
     {
         void INSERT(string table_name, ArrayList listColumns, ArrayList listValue);
         void DELETE(string table_name, string definition = "");
-        List<T> SELECT<T>(string column, string table, string definition = "", string limit = "", string Order_by = "");
+        List<T> SELECT<T>(string columns, string table_name, string definition = "", string Group_by = "", string Order_by = "", string Having = "");
         void UPDATE(string table, string value, string definition);
     }
 }
